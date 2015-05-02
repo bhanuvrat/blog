c046b15d9eabd0579fc148900ddba60e9237cf06
Anuvrat Parashar <anuvrat@anuvrat.in>
2015-05-03 00:43:47 +0530

    more changes for gh pages ...

--- gh-pages-deploy.sh
+++ gh-pages-deploy.sh
@@ -4,5 +4,6 @@ git init
 git config user.name "Travis-CI"
 git config user.email "travis@email.com"
 git add .
+tree
 git commit -m "Deployed to Github Pages"
 git push --force --quiet "https://${GH_TOKEN}@github.com/bhanuvrat/theblog" master:gh-pages > /dev/null 2>&1
