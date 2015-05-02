f2df396ecdfb186d8058093bb4a458dfa38afa4d
Anuvrat Parashar <anuvrat@anuvrat.in>
2015-05-03 04:12:51 +0530

    testing travis-ci echo ...

--- gh-pages-deploy.sh
+++ gh-pages-deploy.sh
@@ -4,6 +4,5 @@ git init
 git config user.name "Travis-CI"
 git config user.email "travis@email.com"
 git add .
-echo "something went wrong"
 git commit -m "Deployed to Github Pages"
 git push --force --quiet "https://${GH_TOKEN}@github.com/bhanuvrat/theblog" master:gh-pages > /dev/null 2>&1
